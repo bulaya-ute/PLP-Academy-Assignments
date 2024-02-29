6e4943948fb60f8d5710dd58b0c3eecca40062e5
Bulaya Mwanaute <68756874+bulaya-ute@users.noreply.github.com>
2024-02-29 22:43:24 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+personalised_message = f"Hello, {name}. Your age is {age} and your location is {location}. Have a great day!"
+print(personalised_message)
